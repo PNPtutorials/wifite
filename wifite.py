76261073ff842552c7054a0b1d7c9b544510ebbb
bettse <bettse@gmail.com>
2011-05-29 06:58:49 +0000

    Replace sql strings with triple-quote strings

--- wifite.py
+++ wifite.py
@@ -46,7 +46,7 @@ except ImportError:
 	print '[!] unable to import tkinter -- GUI disabled'
 
 # current revision
-REVISION=78
+REVISION=79
 
 # default wireless interface (blank to prompt)
 # ex: wlan0, wlan1, rausb0
@@ -1373,18 +1373,18 @@ def logit(txt):
 
 def sqllogit(enc, essid, bssid, key, ascii=""):
 	db = sqlite3.connect('log.db', isolation_level=None)
-	db.execute("CREATE TABLE IF NOT EXISTS log(id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp int, enc text, essid text, bssid text, key text, ascii text)")
-	db.execute("INSERT INTO log (timestamp, enc, essid, bssid, key, ascii) VALUES (%i, '%s', '%s', '%s', '%s', '%s')" % (time.time(), enc, essid, bssid, key, ascii))
+	db.execute("""CREATE TABLE IF NOT EXISTS log(id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp int, enc text, essid text, bssid text, key text, ascii text)""")
+	db.execute("""INSERT INTO log (timestamp, enc, essid, bssid, key, ascii) VALUES (%i, '%s', '%s', '%s', '%s', '%s')""" % (time.time(), enc, essid, bssid, key, ascii))
 	db.close()
 def updatesqlstatus(text):
  	db = sqlite3.connect('log.db', isolation_level=None)
-	db.execute("CREATE TABLE IF NOT EXISTS status(id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp int, status text)")
-	db.execute("INSERT INTO status (timestamp, status) VALUES (%i, '%s')" % (time.time(), text))
+	db.execute("""CREATE TABLE IF NOT EXISTS status(id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp int, status text)""")
+	db.execute("""INSERT INTO status (timestamp, status) VALUES (%i, '%s')""" % (time.time(), text))
 	db.close()
 def updateivps(ivsps):
  	db = sqlite3.connect('log.db', isolation_level=None)
-	db.execute("CREATE TABLE IF NOT EXISTS ivsps(id INTEGER PRIMARY KEY, timestamp int, ivsps int)")
-	db.execute("REPLACE INTO ivsps (id, timestamp, ivsps) VALUES (1, %i, %i)" % (time.time(), ivsps))
+	db.execute("""CREATE TABLE IF NOT EXISTS ivsps(id INTEGER PRIMARY KEY, timestamp int, ivsps int)""")
+	db.execute("""REPLACE INTO ivsps (id, timestamp, ivsps) VALUES (1, %i, %i)""" % (time.time(), ivsps))
 	db.close()
 
 
